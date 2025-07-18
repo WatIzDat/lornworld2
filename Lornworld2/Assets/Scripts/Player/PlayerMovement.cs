3bdc8d6ecb217a0083053b356bf3c3f6855f2fbf
WatIzDat <lily3575@hotmail.com>
2025-07-17 20:53:00 -0700

    Implement basic player movement and sprite

---
+++ Lornworld2/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+    private float moveSpeed = 1;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+#pragma warning disable IDE0051
+    private void OnMove(InputValue inputValue)
+    {
+        Vector2 moveDir = inputValue.Get<Vector2>();
+
+        rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);
+
+        if (moveDir.x > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (moveDir.x < 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+#pragma warning restore IDE0051
+}
