d3069ac7d107397685b89cc6ba41e2da83e29605
WatIzDat <lily3575@hotmail.com>
2025-08-09 20:05:30 -0700

    Implement basic finite state machine

---
+++ Lornworld2/Assets/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+
+public class StateMachine
+{
+    private IState currentState;
+
+    private readonly Dictionary<Type, List<Transition>> transitionsMap = new();
+    private readonly List<Transition> anyTransitions = new();
+    private List<Transition> currentTransitions;
+
+    private static readonly List<Transition> emptyTransitions = new(0);
+
+    public void Tick()
+    {
+        Transition transition = GetTransition();
+
+        if (transition != null)
+        {
+            SetState(transition.ToState);
+        }
+
+        currentState?.Tick();
+    }
+
+    private void SetState(IState state)
+    {
+        if (state == currentState)
+        {
+            return;
+        }
+
+        currentState?.OnExit();
+        currentState = state;
+
+        transitionsMap.TryGetValue(currentState.GetType(), out currentTransitions);
+
+        currentTransitions ??= emptyTransitions;
+
+        currentState.OnEnter();
+    }
+
+    public void AddTransition(IState fromState, IState toState, Func<bool> predicate)
+    {
+        if (!transitionsMap.TryGetValue(fromState.GetType(), out List<Transition> transitions))
+        {
+            transitions = new List<Transition>();
+
+            transitionsMap.Add(fromState.GetType(), transitions);
+        }
+
+        transitions.Add(new Transition(toState, predicate));
+    }
+
+    public void AddAnyTransition(IState toState, Func<bool> predicate)
+    {
+        anyTransitions.Add(new Transition(toState, predicate));
+    }
+
+    private Transition GetTransition()
+    {
+        foreach (Transition transition in anyTransitions)
+        {
+            if (transition.Condition())
+            {
+                return transition;
+            }
+        }
+
+        foreach (Transition transition in currentTransitions)
+        {
+            if (transition.Condition())
+            {
+                return transition;
+            }
+        }
+
+        return null;
+    }
+
+    private class Transition
+    {
+        public IState ToState { get; }
+        public Func<bool> Condition { get; }
+
+        public Transition(IState toState, Func<bool> condition)
+        {
+            ToState = toState;
+            Condition = condition;
+        }
+    }
+}
