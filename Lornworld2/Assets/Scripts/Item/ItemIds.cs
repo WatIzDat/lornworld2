d27e45cfc8cf22c972e62a0f157a33eb4e8d0020
WatIzDat <lily3575@hotmail.com>
2025-11-26 13:54:40 -0800

    Add wood item

--- Lornworld2/Assets/Scripts/Item/ItemIds.cs
+++ Lornworld2/Assets/Scripts/Item/ItemIds.cs
@@ -2,4 +2,5 @@ public static class ItemIds
 {
     public static ItemIdentifier GrassItem { get; set; }
     public static ItemIdentifier StoneItem { get; set; }
+    public static ItemIdentifier WoodItem { get; set; }
 }
