1db9be368d45263c0ed4789d9a6703aa57d1bb54
WatIzDat <lily3575@hotmail.com>
2025-11-29 19:17:10 -0800

    Clean up audio event instances on destroy

--- Lornworld2/Assets/Scripts/Audio/AudioManager.cs
+++ Lornworld2/Assets/Scripts/Audio/AudioManager.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using FMODUnity;
 using FMOD.Studio;
+using System.Collections.Generic;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance { get; private set; }
+
+    private readonly List<EventInstance> eventInstances = new();
 
     private void Awake()
     {
@@ -29,6 +32,22 @@ public class AudioManager : MonoBehaviour
     {
         EventInstance eventInstance = RuntimeManager.CreateInstance(sound);
 
+        eventInstances.Add(eventInstance);
+
         return eventInstance;
+    }
+
+    private void CleanUp()
+    {
+        foreach (EventInstance eventInstance in eventInstances)
+        {
+            eventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            eventInstance.release();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        CleanUp();
     }
 }
