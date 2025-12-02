4f699229d71b862b57ca2791aca7ca2ea0589839
WatIzDat <lily3575@hotmail.com>
2025-12-01 21:38:02 -0800

    Add wooden sword item and crafting recipe

--- Lornworld2/Assets/Scripts/CraftingRecipe/CraftingRecipeIds.cs
+++ Lornworld2/Assets/Scripts/CraftingRecipe/CraftingRecipeIds.cs
@@ -2,4 +2,5 @@ public static class CraftingRecipeIds
 {
     public static CraftingRecipeIdentifier DebugCraftingRecipe { get; set; }
     public static CraftingRecipeIdentifier TorchCraftingRecipe { get; set; }
+    public static CraftingRecipeIdentifier WoodenSwordCraftingRecipe { get; set; }
 }
--- Lornworld2/Assets/Scripts/Item/ItemIds.cs
+++ Lornworld2/Assets/Scripts/Item/ItemIds.cs
@@ -5,4 +5,5 @@ public static class ItemIds
     public static ItemIdentifier WoodItem { get; set; }
     public static ItemIdentifier CoalItem { get; set; }
     public static ItemIdentifier TorchItem { get; set; }
+    public static ItemIdentifier WoodenSwordItem { get; set; }
 }
